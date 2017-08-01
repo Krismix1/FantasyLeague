41decc88bd44886ed82c2ca6f7402d1a5fe72567
eduards <edua0193@stud.kea.dk>
2017-08-01 21:54:19 +0200

    country field added to Footballer class, some more players added

--- team/models.py
+++ team/models.py
@@ -7,6 +7,7 @@ class Footballer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     price = models.FloatField(default=0)
+    country = models.CharField(max_length=30)
     is_injured = models.BooleanField(default=False)
     position = models.CharField(max_length=3)
     real_team = models.CharField(max_length=50)
@@ -15,4 +16,5 @@ class Footballer(models.Model):
     total_points = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return self.first_name + " " + self.last_name + " " + self.real_team + " " + str(self.price)
+        return self.first_name + " " + self.last_name + " " + self.real_team + " " + str(self.price) + " " +\
+               str(self.country)
