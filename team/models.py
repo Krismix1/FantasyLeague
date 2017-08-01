72aab62c24a7f7222cd0b26cba344325e71ce8bc
Epharas <cbetivu@gmail.com>
2017-08-01 14:09:51 +0200

    Add Footballer table to admin section

--- team/models.py
+++ team/models.py
@@ -6,10 +6,13 @@ from django.db import models
 class Footballer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    price = models.FloatField
+    price = models.FloatField(default=0)
     is_injured = models.BooleanField(default=False)
     position = models.CharField(max_length=3)
     real_team = models.CharField(max_length=50)
-    kit_number = models.PositiveSmallIntegerField
-    game_week_points = models.PositiveIntegerField
-    total_points = models.PositiveIntegerField
+    kit_number = models.PositiveSmallIntegerField(default=0)
+    game_week_points = models.PositiveIntegerField(default=0)
+    total_points = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name + " " + self.real_team
